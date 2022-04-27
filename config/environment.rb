a0dfb2dc190c48a1d05229f07fb2daed34e3dd09
ushioda <yusuke_ushioda@mediado.jp>
2022-04-27 13:29:05 +0900

    課題

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: Rails.application.credentials.smtp_user_name,
+  password: Rails.application.credentials.smtp_password,
+  domain: 'admin.example.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
