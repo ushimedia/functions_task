d6fe047e788a44cc638ed69a9e195c952a52d228
ushioda <yusuke_ushioda@mediado.jp>
2022-04-27 13:34:54 +0900

    課題

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.with(to: "test@example.com", name: @user.name ).welcome.deliver_now # 追加
+      UserMailer.with(to: @user.email, name: @user.name ).welcome.deliver_now # 追加
       log_in(@user)
       redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
     else
